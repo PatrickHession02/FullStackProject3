4a1860d7aef6de28105f5f80b38d668eb4a9f3b9
PATRICK HESSION <G00394619@gmit.ie>
2023-12-07 22:01:09 +0000

    Initial commit

---
+++ src/main/java/ie/atu/fullstackproject3/FullStackProject3Application.java
@@ -0,0 +1,13 @@
+package ie.atu.fullstackproject3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackProject3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullStackProject3Application.class, args);
+    }
+
+}
